96332d5296382f59ee20210ed3e15cf6ea84db08
Exequiel65 <marcos97britos@gmail.com>
2024-02-16 10:46:07 -0300

    add layer Domain Core

---
+++ Packgroup.Ecommerce.Domain.Core/CustomerDomain.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Packgroup.Ecommerce.Domain.Entity;
+using Packgroup.Ecommerce.Domain.Interface;
+using PackGroup.Ecommerce.Infrastructura.Interface;
+namespace Packgroup.Ecommerce.Domain.Core
+{
+    public class CustomerDomain : ICustomerDomain
+    {
+        private readonly ICustomerRepository _repository;
+
+        public CustomerDomain(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        #region Métodos Síncronicos
+        public bool Insert(Customers customer)
+        {
+            return _repository.Insert(customer);
+        }
+
+        public bool Úpdate(Customers customer)
+        {
+            return _repository.Update(customer);
+        }
+
+        public bool Delete(string customerId)
+        {
+            return _repository.Delete(customerId);
+        }
+
+        public Customers Get(string customerId)
+        {
+            return _repository.Get(customerId);
+        }
+
+        public IEnumerable<Customers> GetAll()
+        {
+            return _repository.GetAll();
+        }
+        #endregion
+
+        #region Métodos Asíncronos
+        public async Task<bool> InsertAsync(Customers customer)
+        {
+            return await _repository.InsertAsync(customer);
+        }
+
+        public async Task<bool> UpdateAsync(Customers customer)
+        {
+            return await _repository.UpdateAsync(customer);
+        }
+
+        public async Task<bool> DeleteAsync(string customerId)
+        {
+            return await _repository.DeleteAsync(customerId);
+        }
+
+        public async Task<Customers> GetAsync(string customerId)
+        {
+            return await _repository.GetAsync(customerId);
+        }
+
+        public async Task<IEnumerable<Customers>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+        #endregion
+    }
+}
---
+++ Packgroup.Ecommerce.Domain.Interface/ICustomerDomain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Packgroup.Ecommerce.Domain.Entity;
+namespace Packgroup.Ecommerce.Domain.Interface
+{
+    public interface ICustomerDomain
+    {
+        #region Métodos Síncronos
+
+        bool Insert(Customers customer);
+        bool Update(Customers customer);
+        bool Delete(string customerId);
+        Customers Get(string customerId);
+        IEnumerable<Customers> GetAll();
+
+        #endregion
+
+        #region Metodos Asincronicos
+
+        Task<bool> InsertAsync(Customers customer);
+        Task<bool> UpdateAsync(Customers customer);
+        Task<bool> DeleteAsync(string customerId);
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+
+        #endregion
+    }
+}
