b0aac5a66360ab2f913f4214b02734e0e8060e59
Exequiel65 <marcos97britos@gmail.com>
2024-02-24 15:53:10 -0300

    resolve fix to image docker

--- Packgroup.Ecommerce.Services.WebApi/Program.cs
+++ Packgroup.Ecommerce.Services.WebApi/Program.cs
@@ -136,11 +136,11 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors(myPolicy);
 app.UseHttpsRedirection();
