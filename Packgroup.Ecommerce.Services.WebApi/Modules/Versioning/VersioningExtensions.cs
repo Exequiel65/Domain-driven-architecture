99b71c457cbda95430c034e5589dbc656a64bd29
Exequiel65 <marcos97britos@gmail.com>
2024-02-27 10:30:16 -0300

    add versioning for Header

--- Packgroup.Ecommerce.Services.WebApi/Modules/Versioning/VersioningExtensions.cs
+++ Packgroup.Ecommerce.Services.WebApi/Modules/Versioning/VersioningExtensions.cs
@@ -12,7 +12,10 @@ namespace Packgroup.Ecommerce.Services.WebApi.Modules.Versioning
                 o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
                 o.AssumeDefaultVersionWhenUnspecified = true;
                 o.ReportApiVersions = true;
-                o.ApiVersionReader = new QueryStringApiVersionReader("api-version");
+                //versiones por queryString
+                //o.ApiVersionReader = new QueryStringApiVersionReader("api-version");
+                //Versionar por Header
+                o.ApiVersionReader = new HeaderApiVersionReader("x-version");
             });
 
             services.AddVersionedApiExplorer(o =>
